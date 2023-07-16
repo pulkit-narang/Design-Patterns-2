12f6caa531565228b2f3256d5dc5201f96a151c0
PULKIT <pulkit24797@gmail.com>
2023-07-16 13:53:43 +0530

    commit2

--- src/client.java
+++ src/client.java
@@ -2,7 +2,7 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class client {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welco!");
 
 
     }
